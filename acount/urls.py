07b6cb15ee76bf0c29304570a7213aff61479fe1
HRAmbalia <ambaliaharshit25@gmail.com>
2021-03-12 20:34:38 +0530

    First Commit

---
+++ acount/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register/', views.registerUser, name='registerUser'),
+    path('login/', views.loginUser, name='loginUser'),
+    path('home/', views.homePage, name='homePage'),
+    path('logout/', views.logoutUser, name='logoutUser'),
+]
---
+++ acount/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .forms import registerForm
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+# 
+
+def index(request):
+    context = {}
+    return render(request, 'acount/index.html', context)
+
+def registerUser(request):
+    form = registerForm()
+    if request.method == 'POST':
+        form = registerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('loginUser') # 'login' specified in urls.py as name
+    else:
+        form = registerForm()
+    context = {'form':form}
+    return render(request, 'acount/register.html', context)
+
+def loginUser(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('homePage')
+            else:
+                messages.error(request, 'Invalid username or password')
+        else:
+            messages.error(request, 'Fill fields')
+    context = {}
+    return render(request, 'acount/login.html', context)
+
+def homePage(request):
+    context = {}
+    return render(request, 'acount/home.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('index')
