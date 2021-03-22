3616a4dda72721fdbc2539943559b9505ec3da42
HRAmbalia <ambaliaharshit25@gmail.com>
2021-03-22 23:37:27 +0530

    added busDetails db

--- bookTicket/models.py
+++ bookTicket/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from datetime import date
+# 
+
+class busDetails(models.Model):
+    busID = models.AutoField(primary_key=True)
+    arrival = models.CharField(max_length=50)
+    destination = models.CharField(max_length=50)
+    rent = models.IntegerField()
+    distance = models.IntegerField()
+    journeyDate = models.DateField(default=date.today)
+    bookedSeat = models.CharField(max_length=50)
+    time1 = models.TimeField()
+    time2 = models.TimeField()
+
+    def __str__(self):
+        return self.arrival+' --> '+self.destination
